3b443b199308ab7095abfee4da51eaf31c95d21a
alex <aleozlx@126.com>
2014-11-26 10:44:07 +0800

    product detail

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.http import HttpResponse
+from django.views import generic
 from products.models import Product
 
 # def index_view(request):
