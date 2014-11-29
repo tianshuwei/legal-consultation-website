fae832fbef9abc07ff43a6c3696c27ff88d5b783
timothy <648616139@qq.com>
2014-11-29 16:46:19 +0800

    product details

--- products/urls.py
+++ products/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import patterns, url
 from products import views
 
 urlpatterns = patterns('',
-    # url(r'^$', views.index_view, name='index'),
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
-    # url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),,
+	# url(r'^$', views.index_view, name='index'),
+	url(r'^$', views.IndexView.as_view(), name='index'),
+	url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+	url(r'^c(?P<pk>\d+)/$', views.new_comment_view, name='new_comment')
+	# url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),,
 )
 
--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.http import HttpResponse
 from django.views import generic
-from products.models import Product
+from products.models import Product,Comment
 
 # def index_view(request):
 # 	latest_products_list = Product.objects.order_by('-publish_date')
@@ -22,4 +22,18 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
 	model = Product
 	template_name = 'products/detail.html'
+
+	def get_context_data(self, **kwargs):
+		# Call the base implementation first to get a context
+		context = super(DetailView, self).get_context_data(**kwargs)
+		# Add in a QuerySet of all the books
+		context['products_comments'] = Comment.objects.all()
+		return context
+
+def new_comment_view(request, pk):
+	print pk, request.POST['txt_comment']
+	return HttpResponse('template.render(context)')
+
+
+
 
