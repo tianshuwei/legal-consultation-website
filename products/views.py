226b8ec1afa9452dc758871f2528ef5a4879bcc6
timothy <648616139@qq.com>
2014-12-01 21:08:56 +0800

    hahahahahahaha!

--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.template import RequestContext, loader
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views import generic
 from products.models import Product,Comment
+from django.core.urlresolvers import reverse
 
 # def index_view(request):
 # 	latest_products_list = Product.objects.order_by('-publish_date')
@@ -24,15 +25,28 @@ class DetailView(generic.DetailView):
 	template_name = 'products/detail.html'
 
 	def get_context_data(self, **kwargs):
+		print 
+		
 		# Call the base implementation first to get a context
 		context = super(DetailView, self).get_context_data(**kwargs)
 		# Add in a QuerySet of all the books
-		context['products_comments'] = Comment.objects.all()
+		context['products_comments'] = Comment.objects.filter(product_id=kwargs['object'].id)
 		return context
 
 def new_comment_view(request, pk):
-	print pk, request.POST['txt_comment']
-	return HttpResponse('template.render(context)')
+	# print pk, request.POST['txt_comment']
+	p = get_object_or_404(Product, pk=pk)
+	try:
+		# for k in dir(request.user):
+		# 	if 'client' in k:
+		# 		print repr(getattr(request.user,k)),getattr(request.user,k) 
+		comment = Comment.objects.create(comment=request.POST['txt_comment'], client=request.user.client, product=p)
+		comment.save()
+	except :
+		raise
+		#return HttpResponseRedirect(reverse('index:index'))
+	else:
+		return HttpResponseRedirect(reverse('products:detail', args=(p.id,)))
 
 
 
