69de9c6d92f6ae27be4a242197bef759b3a14a47
alex <aleozlx@126.com>
2014-12-02 15:25:18 +0800

    accounts admin c/l filter

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,23 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from accounts.models import Client, Lawyer, Remark, Question, Question_text
 
+class ClientOrLawyerFilter(admin.SimpleListFilter):
+    title = 'client or lawyer'
+    parameter_name = 'CorL'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('clients','Only clients'),
+            ('lawyers','Only lawyers'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value()=='clients':
+            return queryset.select_related('client').filter(client__isnull=False)
+
+        if self.value()=='lawyers':
+            return queryset.select_related('lawyer').filter(lawyer__isnull=False)
+
 class ClientInline(admin.StackedInline):
 	model = Client
 	# can_delete = False
@@ -13,8 +30,9 @@ class LawyerInline(admin.StackedInline):
 	# can_delete = False
 	verbose_name_plural = 'lawyer'
 
-class UserAdmin(UserAdmin):
-	inlines = (ClientInline, LawyerInline)
+class MyUserAdmin(UserAdmin):
+    inlines = (ClientInline, LawyerInline)
+    list_filter = UserAdmin.list_filter + (ClientOrLawyerFilter,)
 
 class Question_textInline(admin.TabularInline):
 	model = Question_text
@@ -24,6 +42,6 @@ class QuestionAdmin(admin.ModelAdmin):
 	inlines = [Question_textInline]
 
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, MyUserAdmin)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Remark)
