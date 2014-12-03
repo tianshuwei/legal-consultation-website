9ebc89c4b1a3201a837518bf9ebec1b2f25a24c4
alex <aleozlx@126.com>
2014-12-03 13:53:45 +0800

    blog comments

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,6 +5,7 @@ from blogs import views
 urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/$', views.IndexView.as_view(), name='index'), # ListView pk->user.id (lawyer)
     url(r'^t(?P<pk>\d+)/$', views.DetailView.as_view(), name='text'), # DetailView
+    url(r'^c(?P<pk>\d+)/$', views.new_comment_view, name='new_comment'),
     url(r'^categories/$', views.categories_view, name='categories'), # ListView
     url(r'^new/$', views.new_article_view, name='new_article'),
 )
--- blogs/views.py
+++ blogs/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext, loader
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ObjectDoesNotExist
 from django import forms
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 from blogs.models import BlogArticle, BlogComment, BlogCategory
 
 def new_article_view(request):
@@ -22,7 +24,22 @@ class DetailView(generic.DetailView):
 	template_name = 'blogs/detail.html'
 	context_object_name = 'article'
 
-def categories_view(request):
-    raise Http404
+	def get_context_data(self, **kwargs):
+		context = super(DetailView, self).get_context_data(**kwargs)
+		context['comments'] = BlogComment.objects.filter(article_id=self.object.id)
+		return context
 
+@login_required
+def new_comment_view(request, pk):
+	p = get_object_or_404(BlogArticle, pk=pk)
+	comment = BlogComment.objects.create(text=request.POST['txt_comment'], user=request.user, article=p)
+	comment.save()
+	return HttpResponseRedirect(reverse('blogs:text', args=(p.id,)))
+
+@login_required
+def categories_view(request):
+	try:
+		return HttpResponseRedirect(reverse('blogs:index',kwargs={'pk':request.user.lawyer.id}))
+	except ObjectDoesNotExist, e:
+		raise Http404
 
