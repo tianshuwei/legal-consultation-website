9caf38aaedf26fa843b6cc942b8cf856c394e5e6
aleozlx <aleozlx@yahoo.com>
2014-12-10 14:02:01 +0800

    url pk具体化

--- products/urls.py
+++ products/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, url
 from products import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index_view, name='index'),
-    url(r'^(?P<pk_product>\d+)/$', views.detail_view, name='detail'),
-    url(r'^c(?P<pk>\d+)/$', views.new_comment_view, name='new_comment'),
-    url(r'^o(?P<pk>\d+)/$', views.new_order_view, name='new_order'),
+	url(r'^$', views.index_view, name='index'),
+	url(r'^(?P<pk_product>\d+)/$', views.detail_view, name='detail'),
+	url(r'^c(?P<pk_product>\d+)/$', views.new_comment_view, name='new_comment'),
+	url(r'^o(?P<pk_product>\d+)/$', views.new_order_view, name='new_order'),
 )
 
--- products/views.py
+++ products/views.py
@@ -3,34 +3,9 @@ from products.models import Product,Comment,Order
 from accounts.models import Lawyer,Client
 from django.views import generic
 
-
-# def index_view(request):
-# 	latest_products_list = Product.objects.order_by('-publish_date')
-# 	template = loader.get_template('products/index.html')
-# 	context = RequestContext(request, {
-# 		'latest_products_list': latest_products_list,
-# 	})
-# 	return HttpResponse(template.render(context))
-
-# class IndexView(generic.ListView):
-# 	template_name = 'products/index.html'
-# 	context_object_name = 'latest_products_list'
-
-# 	def get_queryset(self):
-# 		return Product.objects.order_by('-publish_date')
-
 def index_view(request):
 	return response(request, 'products/index.html',
 		latest_products_list=Product.objects.order_by('-publish_date') )
-
-# class DetailView(generic.DetailView):
-# 	model = Product
-# 	template_name = 'products/detail.html'
-
-# 	def get_context_data(self, **kwargs):
-# 		context = super(DetailView, self).get_context_data(**kwargs)
-# 		context['products_comments'] = Comment.objects.filter(product_id=self.object.id)
-# 		return context
 
 def detail_view(request, pk_product):
 	product=get_object_or_404(Product, pk=pk_product)
@@ -38,31 +13,21 @@ def detail_view(request, pk_product):
 		product=product,
 		products_comments=Comment.objects.filter(product_id=pk_product))
 
-def new_comment_view(request, pk):
-	# print pk, request.POST['txt_comment']
-	p = get_object_or_404(Product, pk=pk)
-	try:
-		# for k in dir(request.user):
-		# 	if 'client' in k:
-		# 		print repr(getattr(request.user,k)),getattr(request.user,k)
-		comment = Comment.objects.create(text=request.POST['txt_comment'], user=request.user, product=p)
-		comment.save()
-	except :
-		raise
-		#return HttpResponseRedirect(reverse('index:index'))
-	else:
-		return HttpResponseRedirect(reverse('products:detail', args=(p.id,)))
+def new_comment_view(request, pk_product):
+	p = get_object_or_404(Product, pk=pk_product)
+	comment = Comment.objects.create(text=request.POST['txt_comment'], user=request.user, product=p)
+	comment.save()
+	return redirect('products:detail', pk_product=p.id)
 
-def new_order_view(request, pk):
-	# raise Http404
-	p = get_object_or_404(Product, pk=pk)
+def new_order_view(request, pk_product):
+	p = get_object_or_404(Product, pk=pk_product)
 	order = Order.objects.create( 
 		client=request.user.client,
 		product=p,
 		lawyer=Lawyer.objects.filter(id=1),
 		state=0,
 		text=request.POST['text']
-		)
+	)
 	order.save()
-	return empty #redirect('products:detail', args=(p.id))
+	return empty
 
