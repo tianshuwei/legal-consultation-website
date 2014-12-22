0ec711532e8c5179906b09767626daec536adb3e
aleozlx <aleozlx@yahoo.com>
2014-12-22 15:40:27 +0800

    fix index

--- index/views.py
+++ index/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
-from django.template import RequestContext, loader
-from django.http import HttpResponse
+# -*- coding: utf-8 -*-
+from org.tools import *
+from accounts.models import Lawyer
 from products.models import Product
 
 def index_view(request):
-	latest_products_list = Product.objects.order_by('-publish_date')[:5]
-	template = loader.get_template('index/index.html')
-	context = RequestContext(request, {
-		'latest_products_list': latest_products_list,
-	})
-	return HttpResponse(template.render(context))
+	return response(request,'index/index.html',
+		latest_products_list=Product.objects.order_by('-publish_date')[:5]
+	)
 
