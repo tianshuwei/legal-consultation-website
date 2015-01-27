7e9de568382e9198829ea3c76094c967848b3818
Alex Yang <aleozlx@126.com>
2015-01-27 16:30:03 +0800

    index url

--- index/urls.py
+++ index/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from index import views
 
 urlpatterns = patterns('',
+	url(r'^$', views.index_view),
     url(r'^index/$', views.index_view, name='index'),
     url(r'^mod/(?P<name>.+)/$', views.mod_view),
     url(r'^transactions/$', views.transaction_record_view),
