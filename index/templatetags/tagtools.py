67cad0e5022ea98c1577a9a54bc47372dce88061
Alex Yang <aleozlx@126.com>
2015-01-30 14:35:06 +0800

    + tagtools

--- index/templatetags/tagtools.py
+++ index/templatetags/tagtools.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
+from org.tools import *
 from django import template
 from django.template.defaultfilters import stringfilter
 register = template.Library()
@@ -13,3 +14,11 @@ def query_assign(request,name,val):
 		if re.search(pattern, querystring): return re.sub(pattern,assignment,querystring)
 		else: return '{0}&{1}'.format(querystring,assignment)
 	except: return ''
+
+TRANSACSERIAL_FILTER_FORMAT = \
+	'<input name="{0}" type="hidden" value="{{0}}" />'.format(TRANSACSERIAL)
+
+@register.filter(name='transacserial',is_safe=True)
+@stringfilter
+def transacserial_filter(transaction_name):
+	return TRANSACSERIAL_FILTER_FORMAT.format(transacserial(transaction_name))
