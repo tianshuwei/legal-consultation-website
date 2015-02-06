226c99c2954b65cad049b87b6917e7a82f882aea
Alex Yang <aleozlx@126.com>
2015-02-06 23:28:37 +0800

    + SC renderer_test.html

--- smartcontract/views.py
+++ smartcontract/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
-from django.template import RequestContext, loader
-from django.http import HttpResponse
+# -*- coding: utf-8 -*-
+from urllib import quote
+from org.docxrenderer import DocxTemplate
+from org.tools import *
+
+mk_disposition = lambda filename: '\x20'.join([ # according to RFC 6266
+	'attachment;',
+	'filename="%s";' % quote(filename),
+	"filename*=%(charset)s'%(lang)s'%(value)s" % {
+		'charset': 'utf-8',
+		'lang': '',
+		'value': quote(filename)
+	}, 
+])
+
+mk_random = lambda l: ''.join(random.choice('QWERTYUIOPASDFGHJKLZXCVBNMZY') for i in xrange(l))
 
 def test_render_view(request):
-	template = loader.get_template('smartcontract/test.html')
-	context = RequestContext(request, {
-		# 'latest_products_list': None,
-	})
-	return HttpResponse(template.render(context))
-
+	if request.method=='POST':
+		template=DocxTemplate(request.FILES['docxtemplate'])
+		r=template.get_vars()
+		if request.POST['test']=='scan':
+			return response(request, 'smartcontract/renderer_test.html',
+				variables=[{'name':var,'count':r[var]} for var in sorted(r)])
+		elif request.POST['test']=='render':
+			for var in r: r[var]=u'["{0}" - 中文 English]'.format(var)
+			doc=template.render(**r)
+			r=HttpResponse(content_type='application/octet-stream')
+			r['Content-Disposition'] = mk_disposition("docxrenderer 测试%s.docx" % mk_random(4))
+			doc.save(r)
+			return r
+		else: raise Http404
+	else: return response(request, 'smartcontract/renderer_test.html')
