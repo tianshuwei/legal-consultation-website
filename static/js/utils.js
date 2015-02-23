9a8a0f418d3b8a2265c9472881d4cf933fe8a27d
timothy <648616139@qq.com>
2015-02-23 19:06:30 +0800

    RSA

--- static/js/utils.js
+++ static/js/utils.js
@@ -180,3 +180,24 @@ function select_nav (suffix) {
 	$("#nav_ul>li").removeClass("active"); 
 	$("#nav_li_"+suffix).addClass("active");
 }
+
+
+function ras_encryption(string){
+	setMaxDigits(19);
+	key = new RSAKeyPair(
+ 		"16d1507964604313b5121c52c1051115",  //e
+ 		"",  
+ 		"70a6c76c3631387e7eaca739f7f5cbe7"   //n
+	);
+	return encryptedString(key,string);
+}
+
+function ras_decryption(en_string){          //测试时用的解密函数，之后可删
+	setMaxDigits(19);
+	key = new RSAKeyPair(
+ 		"16d1507964604313b5121c52c1051115",  //e
+ 		"55afdcf744d8f5fe0c655fee417b3765",  //d
+ 		"70a6c76c3631387e7eaca739f7f5cbe7"   //n
+	);
+	return decryptedString(key,en_string);
+}
