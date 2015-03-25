b43c5d4ac94ee814b4ab50d3342000e0f8ccb3f1
Alex Yang <aleozlx@126.com>
2015-03-25 17:54:32 +0800

    > 关系模式修改 + 全文搜索问题（暂时不包括回答内容） + 获取最新问题 + 获取热门问题

--- questions/models.py
+++ questions/models.py
@@ -1,19 +1,40 @@
 # -*- coding: utf-8 -*-
 from django.db import models, transaction
 from django.contrib.auth.models import User
+from datetime import datetime
 from org.types import Enum
+from org.settings import EN_FULLTEXTSEARCH, POSTGRESQL
+from org.dbtools import rawsql
 
 class EnumQuestionState(Enum):
 	OPEN = 0
 	CLOSED = 1	
 
+class QuestionManager(models.Manager):
+	use_for_related_fields = True
+
+	def search(self, query):
+		if EN_FULLTEXTSEARCH and POSTGRESQL:
+			return self.extra(where=["title@@%s or description@@%s"], params=[query,query]).order_by('-publish_date')
+		else:
+			return self.filter(Q(title__contains=query)|Q(description__contains=query)).order_by('-publish_date')
+
+	def get_latest_questions(self):
+		return self.order_by('-publish_date')[:6]
+
+	def get_popular_questions(self):
+		return self.order_by('-clicks')[:6]
+
 class Question(models.Model):
 	lawyer = models.ForeignKey('accounts.Lawyer', null=True)
 	client = models.ForeignKey('accounts.Client')
 	title = models.CharField(max_length=255)
+	clicks = models.IntegerField(default=0, editable=False)
 	description = models.TextField(blank=True)
-	publish_date = models.DateTimeField(auto_now=True)
+	publish_date = models.DateTimeField(default=datetime.now)
 	state = models.IntegerField(default=EnumQuestionState.OPEN, choices=EnumQuestionState.get_choices())
+
+	objects = QuestionManager()
 
 	def __unicode__(self):
 		return self.title
@@ -35,7 +56,7 @@ class Question_text(models.Model):
 	question = models.ForeignKey(Question)
 	user_flag = models.IntegerField(default=EnumUser_flag.ANSWER, choices=EnumUser_flag.get_choices())
 	text = models.TextField()
-	publish_date = models.DateTimeField(auto_now=True)
+	publish_date = models.DateTimeField(default=datetime.now)
 
 	def __unicode__(self):
 		return self.text[:20]
