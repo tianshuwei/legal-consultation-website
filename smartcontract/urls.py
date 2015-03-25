8374d2be9c734a30680adb831d9a3fe1a8591a9d
tankiJong <tanki_jong@hotmail.com>
2015-03-25 22:42:41 +0800

    修正博客律师登陆样式异常,填充律师列表页,首页破掉的图换掉了. 添加了智能合同首页框架

--- smartcontract/urls.py
+++ smartcontract/urls.py
@@ -4,8 +4,8 @@ from smartcontract import views
 
 urlpatterns = patterns('',
     url(r'^test/$', views.test_render_view, name='test'),
-    url(r'^render_form/(?P<pk_contract>\d+)/$', views.test_render_form_view),
+    url(r'^detail/(?P<pk_contract>\d+)/$', views.test_render_form_view,name='detail'),
     url(r'^test/pdf\.js/(?P<pk_contract>\d+)/$', views.test_pdf_view),
-    url(r'^index/$', views.index_view),
+    url(r'^index/$', views.index_view,name='index'),
 )
 
