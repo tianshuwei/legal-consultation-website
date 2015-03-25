acaab29f7e7f0b1b16a04fc8bc28b1a6aa4e78c1
tankiJong <tanki_jong@hotmail.com>
2015-03-25 21:08:19 +0800

    添加智能文本首页索引, 添加nav的问答标签

--- smartcontract/urls.py
+++ smartcontract/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     url(r'^test/$', views.test_render_view, name='test'),
     url(r'^render_form/(?P<pk_contract>\d+)/$', views.test_render_form_view),
     url(r'^test/pdf\.js/(?P<pk_contract>\d+)/$', views.test_pdf_view),
+    url(r'^index/$', views.index_view),
 )
 
