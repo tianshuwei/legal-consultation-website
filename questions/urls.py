a632bb35c2990f9c456c85e1630bebbfe81c308d
timothy <648616139@qq.com>
2015-03-28 19:09:14 +0800

    add_questions_index

--- questions/urls.py
+++ questions/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
 	url(r'^(?P<pk_question>\d+)/new_text/$', views.new_question_text_view, name='new_question_text'),
 	url(r'^(?P<pk_question>\d+)/satisfied/$',views.question_satisfied,name='satisfied'),
 	url(r'^new/$', views.new_question_view, name='new_question'),
+	url(r'^index/$', views.index_view, name='index'),
 )
 
